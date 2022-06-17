8bc61416a85f096feadcd91f3b8e08e38ee04e8c
steven <steven_jiangxf@hotmail.com>
2022-06-17 17:33:19 +0800

    fix: update

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using Antlr4.Runtime;
 using System;
 using TestAntlr.Parser;
-using static TestAntlr.Parser.SQLiteParser;
 
 namespace TestAntlr
 {
@@ -10,8 +9,8 @@ namespace TestAntlr
         static void Main(string[] args)
         {
             var sql = "SELECT a from t WHERE `id`='am' and m in ('aaa','ssss')";
-            
-           
+
+
             //ICharStream charStream = CharStreams.fromString(sql);
             //var lexer1 = new SQLiteLexer(charStream);
             //ITokenStream stream = new CommonTokenStream(lexer1);
@@ -19,12 +18,10 @@ namespace TestAntlr
             //parser.BuildParseTree = true;
             //ParseContext tree = parser.parse();
             //var lines = tree.sql_stmt_list();
-            
-            CaseChangingCharStream caseCharStream = new CaseChangingCharStream(CharStreams.fromString(sql), true);
-            var lexer2 = new MySqlLexer(caseCharStream);
-            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer2);
-            var mysqlParser = new MySqlParser(commonTokenStream);
-
+            ICharStream charStream= CharStreams.fromString(sql);
+            MySqlLexer lexer = new MySqlLexer(charStream);
+            CommonTokenStream tokenSteam = new CommonTokenStream(lexer);
+            var mysqlParser = new MySqlParser(tokenSteam);
             var data = mysqlParser.root();
 
             Console.WriteLine(data.ToStringTree(mysqlParser));
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 ﻿# Test Antlr
-### 生成代码
-java -jar c:/antlr-4.9.2-complete.jar -Dlanguage=CSharp -package TestAntlr.Parser -no-listener -visitor -o ./Parser MySqlLexer.g4 MySqlParser.g4
